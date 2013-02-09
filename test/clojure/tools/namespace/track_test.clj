b2829ee5d511da9ca0ff2cb4ec6fc92f0043c9bd
David Miller <dmiller2718@gmail.com>
2013-02-09 14:07:26 -0600

    Add track.clj, with tests

---
+++ test/clojure/tools/namespace/track_test.clj
@@ -0,0 +1,30 @@
+(ns clojure.tools.namespace.track-test
+  (:refer-clojure :exclude (remove))
+  (:use clojure.test
+        clojure.tools.namespace.track))
+		
+(deftest t-tracker
+  (let [x (tracker)]
+    (is (empty? x))
+    (is (map? x))))
+  
+(defn- equal-dep-map? [x load unload dependencies depenendents]
+  (= x {:clojure.tools.namespace.track/load   load
+        :clojure.tools.namespace.track/unload unload
+		:clojure.tools.namespace.track/deps  (clojure.tools.namespace.dependency.MapDependencyGraph. dependencies depenendents)}))
+		
+  
+(deftest t-add 
+  (let [y (add (tracker) '{ a #{b} b #{c d}})
+        w (add y '{a #{c} b #{e} d #{f g}})]
+	(is (equal-dep-map? y '(b a) '(b a) '{b #{c d}, a #{b}} '{d #{b}, c #{b}, b #{a}}))
+	(is (equal-dep-map? w '(a d b) '(a b d) '{d #{f g}, b #{e}, a #{c}} '{g #{d}, f #{d}, e #{b}, d #{b}, c #{b a}, b #{a}}))))
+	
+(deftest t-remove
+  (let [y (add (tracker) '{ a #{b} b #{c d}})
+        z (remove y '(d))
+		w (remove y '(q))]
+	(is (equal-dep-map? z  '(b a)  '(a b d) '{b #{c d}, a #{b}} '{d #{b}, c #{b}, b #{a}}))
+	(is (= y w))))
+	
+	
