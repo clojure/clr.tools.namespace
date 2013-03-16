72f65efde59202b8b0919a96ed1db6a89d9e7edf
David Miller <dmiller2718@gmail.com>
2013-03-16 15:16:27 -0500

    Set up packaging for nuget

--- src/clojure/Properties/AssemblyInfo.cs
+++ src/clojure/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("clojure.tools.namespace")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tools for managing namespaces in ClojureCLR.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Miller")]
 [assembly: AssemblyProduct("clojure.tools.namespace")]
