9ef3c9c6662eca998b48f9cce0f29277b072ca77
David Miller <dmiller2718@gmail.com>
2015-02-28 17:39:10 -0600

    Update to Java commit 67c0d68 (2014.12.19): prepare releast tools.namespace-0.2.8

--- src/clojure/Properties/AssemblyInfo.cs
+++ src/clojure/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Miller")]
 [assembly: AssemblyProduct("clr.tools.namespace")]
-[assembly: AssemblyCopyright("Copyright © David Miller 2014")]
+[assembly: AssemblyCopyright("Copyright © David Miller 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.7.0")]
-[assembly: AssemblyFileVersion("0.2.7.0")]
+[assembly: AssemblyVersion("0.2.8.0")]
+[assembly: AssemblyFileVersion("0.2.8.0")]
