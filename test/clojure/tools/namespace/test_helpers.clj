d7416702b3cc9921b469cd7b7db6d588c9d35c39
David Miller <dmiller2718@gmail.com>
2016-08-28 16:58:59 -0500

    Fix bugs in test_helpers

--- test/clojure/tools/namespace/test_helpers.clj
+++ test/clojure/tools/namespace/test_helpers.clj
@@ -1,17 +1,18 @@
 (ns clojure.tools.namespace.test-helpers
   "Utilities to help with testing files and namespaces."
-  (:require [clojure.clr.io :as io]                        ;;; clojure.java.io
+  (:require [clojure.clr.io :as io]                                                      ;;; clojure.java.io
             [clojure.string :as string])
-  (:import (System.IO Path File FileInfo TextWriter Directory)))               ;;; (java.io Closeable File Writer PrintWriter)
+  (:import (System.IO Path File FileInfo DirectoryInfo TextWriter Directory)))           ;;; (java.io Closeable File Writer PrintWriter)
 
 (defn create-temp-dir
   "Creates and returns a new temporary directory java.io.File."
   [name]
-  (let [temp-filename (Path/Combine (Path/GetTempPath) name)]             ;;;  temp-file (File/createTempFile name nil)
-    (File/Delete temp-filename)                                                               ;;; (.delete temp-file)
-    (Directory/Create temp-filename)                                                          ;;; (.mkdirs temp-file)
-    (println "Temporary directory" (Path/GetFullPath temp-filename))                          ;;; (.getAbsolutePath temp-file)
-    temp-filename))                                                                           ;;; temp-file
+  (let [temp-filename (Path/Combine (Path/GetTempPath) name)                   ;;;  temp-file (File/createTempFile name nil)
+        _ (when (or (File/Exists temp-filename)(Directory/Exists temp-filename))
+      		(Directory/Delete temp-filename true))                                ;;; (.delete temp-file)
+       dir (Directory/CreateDirectory temp-filename)]                          ;;; (.mkdirs temp-file)
+    (println "Temporary directory" (Path/GetFullPath temp-filename))           ;;; (.getAbsolutePath temp-file)
+    dir))                                                                      ;;; temp-file
 
 (defn- write-contents
   "Writes contents into writer. Strings are written as-is via println,
@@ -25,18 +26,31 @@
         (prn content))
       (newline))))
 
+;;; DM: Added
+(defn- coerce-to-file-path 
+  "Creates a path (string) from a vector of strings and possible a beginning DirectoryInfo"
+  [path]
+  (let [coerce (fn [x] 
+				(cond
+				  (instance? DirectoryInfo x) (.FullName ^DirectoryInfo x)
+				  (instance? FileInfo x) (.FullName ^FileInfo x)
+				  (instance? String x) x
+				  :otherwise (str x)))]
+	(Path/Combine (into-array String (map coerce path)))))
+;;; DM:
+	  
 (defn create-file
   "Creates a file from a vector of path elements. Writes contents into
   the file. Elements of contents may be data, written via prn, or
   strings, written directly."
   [path contents]
   {:pre [(vector? path)]}
-  (let [name (Path/Combine (into-array String path))]                   ;;; ^File file (apply io/file path)
-                                                                        ;;; (when-let [parent (.getParentFile file)]
-                                                                        ;;; (.mkdirs parent))
-    (with-open [wtr (io/text-writer name)]                              ;;;  io/writer file
+  (let [path (coerce-to-file-path path)                                               ;;; ^File file (apply io/file path)
+        ^FileInfo fi (FileInfo. path)]                                                           ;;; (when-let [parent (.getParentFile file)]
+    (when-not (.Exists fi) (Directory/CreateDirectory (.. fi Directory FullName)))     ;;; (.mkdirs parent))
+    (with-open [wtr (io/text-writer fi)]                                               ;;;  io/writer file
       (write-contents wtr contents))
-    name))                                                              ;;; file
+    fi))                                                                             ;;; file
 
 (defn- sym->path
   "Converts a symbol name into a vector of path parts, not including
